98e15c0ac0e0ef5637e46973bbf462da94125eb8
booyool <863128937@qq.com>
2016-03-24 16:55:56 +0800

    Create getUrlPram.js

---
+++ js/getUrlPram.js
@@ -0,0 +1,4 @@
+  function getURLParameter(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+    }
+    var copOrderNo=getURLParameter("cop_order_no");
